e601e19d2c40d6a7acb93c630e67d3dd7c66f115
Norio Ikeda <nikeda@example.com>
2022-06-30 14:06:40 +0900

    Modified to accept user-defined drawing function.

--- Display_OLED.cpp
+++ Display_OLED.cpp
@@ -22,9 +22,15 @@ void Display_OLED::show_menu(const char *title)
 	snprintf(menu_buf, TMPLEN, "%s", title);
 }
 
-void Display_OLED::display()
+void Display_OLED::display(std::function<void()> f)
 {
 	oled_display.clearDisplay();
+	f();
+	oled_display.display();
+}
+
+void Display_OLED::show_menu()
+{
 	oled_display.setCursor(0, 0);
 	oled_display.print(status_buf);
 
@@ -37,9 +43,54 @@ void Display_OLED::display()
 	oled_display.setCursor(0, 0+TEXT_HEIGHT*3);
 	oled_display.setTextSize(1);
 	oled_display.print(app_msg_buf);
-
-	oled_display.display();
 }
+
+void Display_OLED::print_column(size_t column, const char *buf)
+{
+	oled_display.setCursor(0, column);
+	oled_display.setTextSize(1);
+	oled_display.print(buf);
+}
+
+void Display_OLED::draw_hline(size_t column) {
+	oled_display.drawFastHLine(1, column, 120, WHITE);
+}
+
+void Display_OLED::print_menu(size_t column)
+{
+	oled_display.setCursor(0, column);
+	oled_display.setTextSize(1);
+	oled_display.print(menu_buf);
+}
+
+void Display_OLED::print_app_msg(size_t column)
+{
+	oled_display.setCursor(0, column);
+	oled_display.setTextSize(1);
+	oled_display.print(app_msg_buf);
+}
+
+void Display_OLED::show_graph(const int *arr, size_t len, size_t start)
+{
+	static constexpr const size_t MERGIN = 2; // pixel
+	static constexpr const auto top = 0+TEXT_HEIGHT*3+TEXT_HEIGHT/2 + MERGIN;
+	static constexpr const auto bottom = OLED_HEIGHT - MERGIN;
+	static constexpr const auto hscale = 4096/(bottom-top);
+
+	static constexpr const auto left = MERGIN;
+	static constexpr const auto right = OLED_WIDTH-MERGIN;
+
+	const size_t dx = (right-left)/len;
+	for (size_t i = 0; i < len; ++i) {
+		auto idx = (start+i < len) ? (start+i) : (i+start-len);
+		oled_display.drawFastHLine(left+dx*i, bottom-arr[idx]/hscale, dx, WHITE);
+	}
+}
+/*
+0 1 2 3 4 5 6 7 8 9
+4 5       9 ^ 1 2 3 
+i-(len-start)
+*/
 
 void Display_OLED::test()
 {
--- Display_OLED.h
+++ Display_OLED.h
@@ -6,9 +6,11 @@
 #include <Adafruit_GFX.h>
 #include <Wire.h>
 
-static const char OLED_ADDRESS = 0x3C; //0x78;
-static const size_t OLED_WIDTH = 128;
-static const size_t OLED_HEIGHT = 64;
+#include "ArxTypeTraits.h"
+
+static constexpr const char OLED_ADDRESS = 0x3C; //0x78;
+static constexpr const size_t OLED_WIDTH = 128;
+static constexpr const size_t OLED_HEIGHT = 64;
 
 //  -----------------------------------------------
 
@@ -23,9 +25,9 @@ static const size_t OLED_HEIGHT = 64;
 
 class Display_OLED : public Display
 {
+public:
 	const static size_t TMPLEN = 21;
 
-public:
 	Display_OLED();
 
 	void init();
@@ -33,28 +35,37 @@ public:
 	void show_menu(const char *title) override;
 
 	template <typename... Args>
-	void Display_OLED::show_app_msg(const char *fmt, Args... args)
+	void show_app_msg(const char *fmt, Args... args)
 	{
 		snprintf(app_msg_buf, TMPLEN, fmt, args...);
 	}
 
 	template <typename... Args>
-	void Display_OLED::show_status(const char *fmt, Args... args)
+	void show_status(const char *fmt, Args... args)
 	{
 		snprintf(status_buf, TMPLEN, fmt, args...);
 	}
 
-	void display();
+	void display(std::function<void()> drawing_function);
+
+	void show_menu();
+	void show_graph(const int *arr, size_t len, size_t start);
 
 	void test(); 
+
+	void print_column(size_t column, const char *buf);
+
+	void draw_hline(size_t column); 
+	void print_menu(size_t column);
+	void print_app_msg(size_t column);
+
+	static constexpr size_t TEXT_HEIGHT = 8;
+	static constexpr size_t TEXT_WIDTH = 6;
 
 private:
 	char menu_buf[TMPLEN];
 	char app_msg_buf[TMPLEN];
 	char status_buf[TMPLEN];
-
-	static constexpr size_t TEXT_HEIGHT = 8;
-	static constexpr size_t TEXT_WIDTH = 6;
 
 	Adafruit_SSD1306 oled_display;
 };
